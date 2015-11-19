38e727c297462e6cad7584e4e8e268397bbf6796
VUJURU DINESH <dinesh1729v@gmail.com>
2015-11-19 14:44:33 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -17,8 +17,43 @@ NOTES: For negative numbers ignore negative sign.
 struct node {
 	int num;
 	struct node *next;
-};
-
+}*top,*temp;
+int reverse(int n)
+{
+	int rev = 0;
+	while (n > 0)
+	{
+		rev = rev * 10 + n % 10;
+		n /= 10;
+	}
+	return rev;
+}
+void push(int data)
+{
+	if (top == NULL)
+	{
+		top = (struct node *)malloc(1 * sizeof(struct node));
+		top->next = NULL;
+		top->num = data;
+	}
+	else
+	{
+		temp = (struct node *)malloc(1 * sizeof(struct node));
+		temp->next = top;
+		temp->num = data;
+		top = temp;
+	}
+}
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	//N = reverse(N);
+	top = NULL;
+	if (N<0)N *= -1;
+	if (N == 0)push(0);
+	else
+	while (N > 0)
+	{
+		push(N % 10);
+		N /= 10;
+	}
+	return top;
 }
