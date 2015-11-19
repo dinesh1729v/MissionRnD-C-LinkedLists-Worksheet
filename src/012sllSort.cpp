378fc5109225caecb9513353112b63226f7000a4
VUJURU DINESH <dinesh1729v@gmail.com>
2015-11-19 15:25:25 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -20,7 +20,33 @@ struct node {
 	int data;
 	struct node *next;
 };
-
 void sll_012_sort(struct node *head){
-	
+	int cnt = 0,cnt0=0,cnt1=0,cnt2=0,sum=0,i;
+	struct node *start = head;
+	while (head != NULL)
+	{
+		if (head->data == 1)cnt1++;
+		sum += head->data;
+		head = head->next;
+		cnt++;
+	}
+	cnt2 = (sum - cnt1) / 2;
+	cnt0 = (cnt - cnt2 - cnt1);
+	head = start;
+	for (i = 0; i < cnt0; i++)
+	{
+		head->data = 0;
+		head = head->next;
+	}
+	for (i = 0; i < cnt1; i++)
+	{
+		head->data = 1;
+		head = head->next;
+	}
+	for (i = 0; i < cnt2; i++)
+	{
+		head->data = 2;
+		head = head->next;
+	}
+	head = NULL;
 }
